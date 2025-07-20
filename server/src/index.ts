eff5bcb9384357d4feefeac05e6ecc799d62df62
Pouya Hendy <pouya.hedy@gmail.com>
2025-07-20 19:35:06 +0330

    timestamp 2:46:00

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,6 +7,7 @@ import morgan from "morgan";
 import { Server } from "http";
 // Route Imports
 import projectRoutes from "./routes/projectRoutes";
+import taskRoutes from "./routes/taskRoutes";
 
 // configurations
 dotenv.config();
@@ -25,6 +26,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/projects", projectRoutes);
+app.use("/tasks", taskRoutes);
 
 // server
 const port = process.env.PORT || 3000;
